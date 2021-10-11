e29eb9882d4764f476195a4215a18b16961e90f1
yForecasting <yves.sagaert@gmail.com>
2021-10-11 12:00:53 +0200

    initial commit to r package

---
+++ R/dds_package.R
@@ -0,0 +1,9 @@
+#' dds: Descriptive Data Science
+#'
+#' This package provides descriptive data analysis
+#' in addition to your standard data plotting. 
+#'
+#' @author  Yves R. Sagaert \email{yves.r.sagaert@gmail.com}
+#' @docType package
+#' @name dds
+"_PACKAGE"
