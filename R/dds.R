63c9e90592945662c23a09786034b84a646608e7
yForecasting <yves.sagaert@gmail.com>
2021-10-13 15:36:14 +0200

    Add analysis for residuals (res) and acf

--- R/dds.R
+++ R/dds.R
@@ -5,7 +5,9 @@
 #' This function converts graphical output to textual and auditive output.
 #'
 #' @param x Data or output of a function which needs description
-#' @param fun Function that needs to be executed on the data (optional)
+#' @param fun Function that needs to be executed on the data (optional).
+#'            Argument fun can be: c("ts","res", "acf"). See examples for
+#'            individual use of fun (x is changing alongside)
 #' @param a Auditive output required (default = TRUE)
 #' #'
 #' @author Yves R. Sagaert
@@ -21,23 +23,47 @@
 #'      dds(x)
 #'   }
 #'
+#' # Any data
+#' set.seed(1010)
 #' x <- rnorm(100)
 #' dds(x,a=FALSE)
 #'
+#' # Time series data
+#' set.seed(1010)
+#' x <- ts(rnorm(100),start=c(2020,1),freq=12)
+#' dds(x,a=FALSE,fun="ts")
+#'
+#' # Residual analysis
+#' library(forecast)
+#' set.seed(1010)
+#' x <- ts(rnorm(100),start=c(2020,1),freq=12)
+#' dds(naive(x),a=FALSE,fun="res")
 
 
 dds <- function(x, fun = "", a = TRUE){
-  
+
   # Not the additional function information
   if (a==TRUE){sonify::sonify(x)}
-  cat(paste0("The data contains ", length(x), " points with mean ",
-             round(mean(x, na.rm = TRUE), 1), ", variance ",
-             round(stats::var(x, na.rm = TRUE), 1), ".\n"))
-  
+
+
+  # Do additional stuff if: residuals data
+  if (fun=="res"){
+    # Make a time series of the residuals
+    # x is forecast model here ==> new e is ts
+    e <- stats::ts(stats::residuals(x)[-1],start=stats::start(x$x),freq=stats::frequency(x$x))
+    cat("Residual analysis. ")
+    dds::dds(e,a=FALSE,fun="ts") # getting real recursive here :P
+    dds::dds(e,a=FALSE,fun="acf")
+  }
+
   if (!(fun=="")){
     # Additional info on function goes here
+
     if (fun=="ts"){
       # Description of the time series data
+      cat(paste0("The data contains ", length(x), " points with mean ",
+                 round(mean(x, na.rm = TRUE), 1), ", variance ",
+                 round(stats::var(x, na.rm = TRUE), 1), ".\n"))
       # Cox and Stuart Trend test
       trend <- dds::condition((trend::cs.test(x)$p.value<0.05),
                               "a monotonic trend",
@@ -61,5 +87,16 @@ dds <- function(x, fun = "", a = TRUE){
       cat(paste0("Moreover it has ", changepoint,
                  ". "))
     }
+    if (fun=="acf"){
+      # Symbolic representation of ggAcf
+      i <- forecast::ggAcf(x,lag.max=18) # max 18 for readability
+      acf <- sign(i$data$Freq)*(abs(i$data$Freq)>0.24)
+      cat("ACF representation of significant lags: ")
+      cat(acf)
+    }
+  } else {
+    cat(paste0("The data contains ", length(x), " points with mean ",
+               round(mean(x, na.rm = TRUE), 1), ", variance ",
+               round(stats::var(x, na.rm = TRUE), 1), ".\n"))
   }
 }
